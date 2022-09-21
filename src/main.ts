5b8012119276238f5605567940bda6d52f97b59b
windilycloud <wwww.4399li@qq.com>
2022-09-21 23:23:04 +0800

    fix: add database option type

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,19 @@ import Database from 'better-sqlite3'
 import { SqliteError } from 'better-sqlite3'
 
 export default class SqliteDatabase extends Plugin {
-  SqliteDatabase: (sqlitePath: string, options: any) => any
+  SqliteDatabase: (sqlitePath: string, options?: Database.Options) => any
   SqliteError: SqliteError
   async onload(): Promise<void> {
-    console.log('loading sample plugin...')
+    console.log('loading obsidian-sqlite3 plugin...')
 
     this.SqliteDatabase = this.databaseWrapper
+    this.SqliteError = SqliteError
   }
-  databaseWrapper(sqlitePath: string, options: any) {
+  databaseWrapper(sqlitePath: string, options?: Database.Options) {
     return new Database(sqlitePath, options)
   }
 
   async onunload(): Promise<void> {
-    console.log('unloading plugin')
+    console.log('unloading obsidain-sqlite3 plugin')
   }
 }
